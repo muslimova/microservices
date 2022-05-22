49fb547d56f2ddd79a7f883c7130f5311c817d7b
zilya <zilyamuslimova3@gmail.com>
2022-05-22 12:57:33 +0400

    .

---
+++ UdsuMicroservices/auth-service/src/main/java/com/udsu/auth/service/UserService.java
@@ -0,0 +1,9 @@
+package com.udsu.auth.service;
+
+import com.udsu.auth.domain.User;
+
+public interface UserService {
+
+    void create(User user);
+
+}
---
+++ UdsuMicroservices/auth-service/src/main/java/com/udsu/auth/service/secure/MyUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.udsu.auth.service.secure;
+
+import com.udsu.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return repository.findById(username).orElseThrow(()->new UsernameNotFoundException(username));
+    }
+}
